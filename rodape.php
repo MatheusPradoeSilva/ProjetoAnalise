0cecec833e232e553109e9875b775d0ef5f4d5fe
Matheus Prado <pradoesilvamatheus@gmail.com>
2022-08-17 23:48:33 -0300

    carrossel e botao menu funcional

--- rodape.php
+++ rodape.php
@@ -1,43 +1,49 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 <link rel="stylesheet" type="text/css" href="cssanalise.css" />
     
 <nav>
-<ul class="container">
-  <form action = "TelaLogin.php" method = "POST">
-<input type="image" class = "menubot" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAAV0lEQVRIie3SsQ2AMBADQD9zoOwDg7C4B3GaREJRJES+gMLXfWN/YcDekHRKotZR0nHPjKGAAPbkn4yI0o8tGfZoLLgAMJHHlmGLPNMZz/RnPNMZz/R7FeMv6QUDfUW1AAAAAElFTkSuQmCC">
-</form>
-<li class='dropdown'>
-<a href='#home'>Home <i class="fa fa-angle-down"></i></a>
-
-<div class='mega-menu'>
-    <div class="container">
-      <div class="item">
-          <ul>
-            <li><a href='index.php'>Início</a></li>
+<ul class="container">                                    
+  <div class="dropdown">
+    <input type="image" class= "menubot" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAAV0lEQVRIie3SsQ2AMBADQD9zoOwDg7C4B3GaREJRJES+gMLXfWN/YcDekHRKotZR0nHPjKGAAPbkn4yI0o8tGfZoLLgAMJHHlmGLPNMZz/RnPNMZz/R7FeMv6QUDfUW1AAAAAElFTkSuQmCC" class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
+   
+    <ul class="dropdown-menu">
+      <?php session_start(); 
+       $logado = $_SESSION['email'];
+      echo " <li class='dropdown'>
+      <div class='mega-menu'>
+          <div class='container'>
+            <div class='item'>
+      
+      <li>Bem vindo $logado</li>
+    <li><a href='index.php'>Início</a></li>
             <li><a href='minhaconta.php'>Minha conta</a></li>
             <li><a href='#'>Regras do site</a></li>
             <li><a href='#'>Minhas publicações</a></li>
             <li><a href='#'>Publicar</a></li>
-            <li><button type="submit">Sair da conta</li>
-          </ul>
-      </div>
-    <li class='dropdown'>
-  <a href='#categ'>Categorias <i class="fa fa-angle-down"></i></a>
-  <div class='mega-menu'>
-    <div class="container">
-      <div class="item">
-          <ul>
+            <li class = 'linamenu'>________________</li>
+            <li><h5>CATEGORIAS<h5></li>
             <li><a href='#'>Arte</a></li>
             <li><a href='#'>Música</a></li>
             <li><a href='#'>Esportes</a></li>
             <li><a href='#'>Culinária</a></li>
             <li><a href='#'>Famosos</a></li>
             <li><a href='#'>Pets</a></li>
-          </ul>
-      </div>
+            <li><button type='submit'>Sair da conta</li>
+            <div>" ?>
+    
+    </ul>
+  </div>
+</div>
+<li class='dropdown'>
+<a href='#home'>Home <i class="fa fa-angle-down"></i></a>
+
    
 </ul>
 
