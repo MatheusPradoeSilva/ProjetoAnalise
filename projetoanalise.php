e76a65463fa4a551219b12bed646089bcba4ab75
Matheus Prado <pradoesilvamatheus@gmail.com>
2022-09-29 13:36:09 -0300

    alteração aline livia

---
+++ projetoanalise.php
@@ -0,0 +1,28 @@
+<?php
+include('rodape.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Cache-Control" content="no-store" />
+<meta name="viewport" content="width-device-width">
+
+</head>
+<body>
+
+    <h2>Compartilhe conosco!</h2>
+    <h4>Venha se juntar á nós e compartilhe já seus<br>
+     artigos, ideais, receitas e muito mais.
+     <br><br>
+    Publique no botão abaixo e seja feliz!</h4>
+
+    <button>Publicar</button>
+
+</body>
+
+
+</html>
+
---
+++ rodape.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<meta charset="utf-8">
+
+<link rel="stylesheet" type="text/css" href="cssanalise.css" />
+    
+<nav>
+<ul class="container">
+<li class='dropdown'>
+<a href='#home'>Home <i class="fa fa-angle-down"></i></a>
+<div class='mega-menu'>
+    <div class="container">
+      <div class="item">
+          <ul>
+            <li><a href='projetoanalise.php'>Início</a></li>
+            <li><a href='#'>Minha conta</a></li>
+            <li><a href='#'>Regras do site</a></li>
+            <li><a href='#'>Minhas publicações</a></li>
+            <li><a href='#'>Publicar</a></li>
+            <li><button type="submit">Sair da conta</li>
+          </ul>
+      </div>
+    <li class='dropdown'>
+  <a href='#categ'>Categorias <i class="fa fa-angle-down"></i></a>
+  <div class='mega-menu'>
+    <div class="container">
+      <div class="item">
+          <ul>
+            <li><a href='#'>Arte</a></li>
+            <li><a href='#'>Música</a></li>
+            <li><a href='#'>Esportes</a></li>
+            <li><a href='#'>Culinária</a></li>
+            <li><a href='#'>Famosos</a></li>
+            <li><a href='#'>Pets</a></li>
+          </ul>
+      </div>
+   
+</ul>
+
+      <input type="text" name="busc"><button type="submit"><i class="fa fa-search"></i>x</button>
+
+</nav>
